a95b38d6b520b22e16c10f46bc2d1d8d5754d5d2
Ryan Janke <jankersr@yahoo.com>
2016-09-13 01:41:40 -0600

    Added ico file and thumbs up.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,84 +1,65 @@
-// window.onload = function() {
-//
-//   // get the button to flip the card
-//   var flipButton = document.getElementById("flip-button");
-//
-//   flipButton.onclick = function() {
-//     document.querySelector("#flip").classList.toggle("flip");
-//   };
-//
-//
-// };
-// function flip(event) {
-//   document.querySelector("#flip").classList.toggle("flip");
-// };
-// document.addEventListener('DOMContentLoaded', function() {
-//   var cards = document.querySelectorAll(".amendment-card");
-//   var flipButtons = document.querySelectorAll("#flip-button");
-//   var flip = document.querySelector(".flip-container");
-//
-//   console.log("Flip button: " + flipButtons.length);
-//   console.log("Card: " + cards.length);
-//
-//   for (var i = 0; i < cards.length; i++) {
-//     flipButtons[i].addEventListener("click", function(event) {
-//       var elem = flipButtons[i];
-//       console.log(elem);
-//     });
-//   };
-//
-//   document.body.addEventListener("click", function(event) {
-//     if (event.target.nodeName == "BUTTON")
-//       flip.classList.toggle("flip");
-//   });
-// }, false);
-
 // Makes sure the DOM is ready
 $(document).ready(function() {
 
+  // Get variables for the DOM to speed it up
+  var openAll = $(".open-all");
+  var closeAll = $(".close-all");
+  var flipContainer = (".flip-container");
+  var top = $(".top");
+  var htmlBody = $("html, body");
+  var wat = $(".wat");
+  var watModal = $(".wat-modal");
+  var watModalClose = $(".wat-modal, close");
+  var no = $(".no");
+  var amendmentCard = $(".amendment-card");
+  var button = $(".button");
+  var thumbsUp = $(".thumbs-up");
+
+
   // Open all of the cards to show amendment definition
-  $(".open-all").on("click", function() {
-    $(".flip-container").each(function() {
+  $(openAll).on("click", function() {
+    $(flipContainer).each(function() {
       $(this).addClass("flip");
     });
   });
 
   // Closes all of the .amendment-card cards
-  $(".close-all").on("click", function() {
-    $(".flip-container").each(function() {
+  $(closeAll).on("click", function() {
+    $(flipContainer).each(function() {
       $(this).removeClass("flip");
     });
   });
 
   // Scrolls the page to the top
-  $('.top').click(function(){
-    $('html, body').animate({scrollTop : 0},1250);
+  $(top).click(function(){
+    $(htmlBody).animate({scrollTop : 0},1250);
     return false;
   });
 
   // Wat modal code
-  $(".wat").on("click", function() {
-    $(".wat-modal").css("display", "block");
+  $(wat).on("click", function() {
+    $(watModal).css("display", "block");
   });
 
   // On click closes the .wat-modal if .wat-modal is clicked
-  $(".wat-modal, .close").on("click", function(event) {
+  $(watModalClose).on("click", function(event) {
     if (!$(event.target).is(".wat-modal, .close")) {
       return;
     } else {
-        $(".wat-modal").css("display", "none");
+        $(watModal).css("display", "none");
     }
   });
 
   // Function for 'No' button
   // Gets each .button.no elements
-  $(".no").each(function() {
+  $(no).each(function() {
 
     // for whatever .flip-button class is clicked a click event is hooked to it
     $(this).on("click", function() {
 
       // finds closest .amendment-card card finds .flip-container to toggle the class
-      $(this).closest(".amendment-card").find(".flip-container").toggleClass("flip");
+      $(this).closest(amendmentCard).find(flipContainer).toggleClass("flip");
+      $(this).closest(amendmentCard).find(thumbsUp).animate({"opacity": "0", "height": "0"}, 450);
     });
   });
 
@@ -90,13 +71,15 @@ $(document).ready(function() {
     $(this).on("click", function() {
 
       // finds closest .amendment-card card finds .flip-container to remove the class
-      $(this).closest(".amendment-card").find(".flip-container").removeClass("flip");
+      $(this).closest(amendmentCard).find(flipContainer).removeClass("flip");
+      $(this).closest(amendmentCard).find(thumbsUp).animate({"opacity": "1", "height": "152px"}, 450);
+
     });
   });
 
 
-  // Changes the CSS of the .button class to make it look liek its being clicked
-  $(".button").each(function() {
+  // Changes the CSS of the .button class to make it look like its being clicked
+  $(button).each(function() {
 
     // mousedown removes the box shadow
     $(this).mousedown(function() {
